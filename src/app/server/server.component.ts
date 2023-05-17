a630074ada6ca0ea9dec0b19b14b722fb1c0367e
Sagar Dighe <sagasoftech@gmail.com>
2023-05-17 02:52:18 -0500

    Display app-server with random status

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,10 @@ export class ServerComponent{
     serverId = 10;
     serverStatus = 'Offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5? 'Online':'Offline'
+    }
+    
     getServerStatus(){
         return this.serverStatus;
     }
