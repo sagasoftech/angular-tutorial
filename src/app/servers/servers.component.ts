720cf10be2e080fa1ed1008bec4397a1104d4db7
Sagar Dighe <sagasoftech@gmail.com>
2023-05-17 01:12:38 -0500

    Property binding example: Enable/Disable button after timeout with property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
 
+  allowNewServer = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
