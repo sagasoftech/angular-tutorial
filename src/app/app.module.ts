e6c5d1d1ea4f63dcacddb57d5e8c4bae046285a8
Sagar Dighe <sagasoftech@gmail.com>
2023-05-17 02:33:57 -0500

    Assignment 1: 1. Add a Input field which updates a property ('username') via Two-Way-Binding 2. Output the username property via String Interpolation (in a paragraph below the input) 3. Add a button which may only be clicked if the username is NOT an empty string 4. Upon clicking the button, the username should be reset to an empty string

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
+import { Assignment1Component } from './assignment1/assignment1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    Assignment1Component
   ],
   imports: [
     BrowserModule,
