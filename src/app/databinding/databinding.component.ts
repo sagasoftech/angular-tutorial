8e770216393ff7329a2ae48bcd37c1ebc3c8ffa0
Sagar Dighe <sagasoftech@gmail.com>
2023-05-18 23:33:52 -0500

    Create custom event in Parent Component

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 export class DatabindingComponent {
   serverElements = [{name: 'TestServer', type: 'server', content: 'This is a Test Server'}];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
+
 }
