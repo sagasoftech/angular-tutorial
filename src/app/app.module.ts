dfeb008fbee11de1c3764e3042453178c052abce
Sagar Dighe <sagasoftech@gmail.com>
2023-05-26 03:41:17 -0500

    Assignment 4: 1. Create three new components: GameControl, Odd and Even 2. The GameControl Component should have buttons to start and stop the game 3. When starting the game, an event (holding a incrementing number) should get emitted each second (ref = setInterval()) 4. The event should be listenable from outside the component 5. When stopping the game, no more events should get emitted (clearInterval(ref)) 6. A new Odd component should get created for every odd number emitted, the same should happen for the Even Component (on even numbers) 7. Simply output Odd - NUMBER or Even - NUMBER in the two components 8. Style the element (e.g. paragraph) holding your output text differently in both components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { Assignment3Component } from './assignment3/assignment3.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { CockpitComponent } from './databinding/cockpit/cockpit.component';
 import { ServerElementComponent } from './databinding/server-element/server-element.component';
+import { Assignment4Component } from './assignment4/assignment4.component';
+import { GameControlComponent } from './assignment4/game-control/game-control.component';
+import { OddComponent } from './assignment4/odd/odd.component';
+import { EvenComponent } from './assignment4/even/even.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { ServerElementComponent } from './databinding/server-element/server-elem
     Assignment3Component,
     DatabindingComponent,
     CockpitComponent,
-    ServerElementComponent
+    ServerElementComponent,
+    Assignment4Component,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
