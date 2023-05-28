6c2eb95adc1e1e1949a413e05d80b2e04077c4c6
Sagar Dighe <sagasoftech@gmail.com>
2023-05-28 17:19:53 -0500

    Injecting LoggingService into Components: Alternate syntax by using the inject() function

--- src/app/services/account/account.component.ts
+++ src/app/services/account/account.component.ts
@@ -1,18 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject  } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  private loggingService?: LoggingService; // <- must be added
+
+  constructor(){
+    this.loggingService = inject(LoggingService);
+  }
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
