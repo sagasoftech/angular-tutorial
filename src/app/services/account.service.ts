7bc88d2da785e40feb06549b1397cd3767e10912
Sagar Dighe <sagasoftech@gmail.com>
2023-05-29 00:01:02 -0500

    Injecting services into another services: Still it will not work we have to inject it

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,3 +1,5 @@
+import { LoggingService } from "../logging.service";
+
 export class AccountService{
 
     accounts = [
@@ -15,12 +17,16 @@ export class AccountService{
         }
       ];
 
+    constructor(private loggingService: LoggingService) {}
+    
     addAccount(name: string, status: string) {
-    this.accounts.push({name: name, status: status});
+        this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
     }
 
     updateStatus(id: number, newStatus: string) {
-    this.accounts[id].status = newStatus;
+        this.accounts[id].status = newStatus;
+        this.loggingService.logStatusChange(newStatus);
     }
     
 }
