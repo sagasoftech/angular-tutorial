e7b17d90ea1ea88ca6daa9963dadd0d672a15d74
Sagar Dighe <sagasoftech@gmail.com>
2023-05-29 01:47:03 -0500

    Assignment 5: Solution

--- src/app/assignment5/active-users/active-users.component.ts
+++ src/app/assignment5/active-users/active-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+export class ActiveUsersComponent implements OnInit{
+  users: string[];
+  userChanges: number = 0;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.usersService.activeUsers;
+  }
 
   onSetToInactive(id: number){
-    this.userSetToInactive.emit(id);
+    this.usersService.setToInactive(id);
+    this.userChanges = this.usersService.getActiveToInactiveCount();
   }
 }
--- src/app/assignment5/inactive-users/inactive-users.component.ts
+++ src/app/assignment5/inactive-users/inactive-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+export class InactiveUsersComponent implements OnInit{
+  users: string[];
+  userChanges: number = 0;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.usersService.inactiveUsers;
+  }
 
   onSetToActive(id: number){
-    this.userSetToActive.emit(id);
+    this.usersService.setToActive(id);
+    this.userChanges = this.usersService.getInactiveToActiveCount();
   }
 }
