6cbdd480a16f6dbe72731df884328cbf5a00f9e4
Sagar Dighe <sagasoftech@gmail.com>
2023-05-30 00:07:01 -0500

    Code formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,19 +8,24 @@ import { RoutingServersComponent } from "./routing/routing-servers/routing-serve
 import { RoutingUserComponent } from "./routing/routing-users/routing-user/routing-user.component";
 import { RoutingUsersComponent } from "./routing/routing-users/routing-users.component";
 import { RoutingHomeComponent } from "./routing/routing-home/routing-home.component";
+import { AuthGuard } from "./auth-guard";
 
 const appRoutes: Routes = [
-    {path: '', component: RoutingHomeComponent},
-    {path: 'users', component: RoutingUsersComponent, children: [
-      {path: ':id/:name', component: RoutingUserComponent}
-    ]},
-    {path: 'servers', component: RoutingServersComponent, children: [
-      {path: ':id', component: RoutingServerComponent},
-      {path: ':id/edit', component: RoutingEditServerComponent}
-    ]},
-    {path: 'not-found', component: PageNotFoundComponent},
-    {path: '**', redirectTo: '/not-found'}
-  ]
+  { path: '', component: RoutingHomeComponent },
+  {
+    path: 'users', component: RoutingUsersComponent, children: [
+      { path: ':id/:name', component: RoutingUserComponent }
+    ]
+  },
+  {
+    path: 'servers', component: RoutingServersComponent, children: [
+      { path: ':id', component: RoutingServerComponent },
+      { path: ':id/edit', component: RoutingEditServerComponent }
+    ]
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+]
 
 @NgModule({
     imports: [
